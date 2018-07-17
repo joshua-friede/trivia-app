bfc7ab9dcfbb8a5fcef05dd7a950980123cd7d1c
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-17 14:47:26 -0500

    initial commit

---
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/Finished.kt
@@ -0,0 +1,14 @@
+package tech.friede.joshua.trivia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Finished : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finished)
+    }
+
+
+}
