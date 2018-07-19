72976977aa3b9fcc50f74445789d040e72a8e211
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-19 16:10:01 -0500

    create longer quiz

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/controller/Session.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/controller/Session.kt
@@ -19,8 +19,12 @@ object Session {
 
         val dummyQuiz = Quiz("dummyQuiz")
         val a = TrueFalseAnswer("dummyQuestion1", true)
-        val dummyQuestion = TrueFalseTriviaQuestion("dummyQuestion1", "The sky is blue:", a)
-        dummyQuiz.addQuestion(dummyQuestion)
+        val dummyQuestion1 = TrueFalseTriviaQuestion("dummyQuestion1", "The sky is blue:", a)
+        dummyQuiz.addQuestion(dummyQuestion1)
+        val dummyQuestion2 = TrueFalseTriviaQuestion("dummyQuestion2", "The earth is round:", a)
+        dummyQuiz.addQuestion(dummyQuestion2)
+        val dummyQuestion3 = TrueFalseTriviaQuestion("dummyQuestion3", "Kotlin is the best language:", a)
+        dummyQuiz.addQuestion(dummyQuestion3)
         Storage.createQuiz(dummyQuiz)
 
         selectedQuizName = "dummyQuiz"
