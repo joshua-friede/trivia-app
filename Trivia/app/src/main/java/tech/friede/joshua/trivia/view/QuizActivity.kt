0a5d28fce21f360f494be0cb6acf3a408952a621
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-19 12:13:47 -0500

    create superclass implementing next question

---
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/view/QuizActivity.kt
@@ -0,0 +1,23 @@
+package tech.friede.joshua.trivia.view
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import tech.friede.joshua.trivia.controller.Session
+import tech.friede.joshua.trivia.model.MultipleChoiceTriviaQuestion
+import tech.friede.joshua.trivia.model.TrueFalseTriviaQuestion
+
+abstract class QuizActivity: AppCompatActivity() {
+
+    fun next(v: View) {
+        Session.currentQuestion++
+        val i = Intent()
+
+        when( Session.selectedQuiz.getQuestionN(Session.currentQuestion) ) {
+            is TrueFalseTriviaQuestion -> i.setClass(this, TrueFalseQuestionActivity::class.java)
+            is MultipleChoiceTriviaQuestion -> i.setClass(this, MultipleChoiceTriviaQuestion::class.java)
+            else -> i.setClass(this, Finished::class.java)
+        }
+        startActivity(i)
+    }
+}
