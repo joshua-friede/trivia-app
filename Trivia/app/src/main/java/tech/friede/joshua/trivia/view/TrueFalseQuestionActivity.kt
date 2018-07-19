eb80f268ea9cfa26907503736475027e52f839bb
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-19 11:02:21 -0500

    dynamically set MC question text

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/view/TrueFalseQuestionActivity.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/view/TrueFalseQuestionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_true_false_question.*
 import tech.friede.joshua.trivia.R
 
 class TrueFalseQuestionActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class TrueFalseQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_true_false_question)
+
+        tf_question.text = MainActivity.selectedQuiz.questions[MainActivity.currentQuestion].question
     }
 
     fun next(v: View) {
