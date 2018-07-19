d9fd1fd66ed58d143dd3c0a1c50b96e572a1b5de
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-19 15:32:13 -0500

    remove backend from session object

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/controller/Session.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/controller/Session.kt
@@ -2,10 +2,29 @@ package tech.friede.joshua.trivia.controller
 
 import tech.friede.joshua.trivia.model.Quiz
 import tech.friede.joshua.trivia.model.QuizResponse
+import tech.friede.joshua.trivia.model.TrueFalseAnswer
+import tech.friede.joshua.trivia.model.TrueFalseTriviaQuestion
+
 
 object Session {
+
+    lateinit var selectedQuizName: String
     lateinit var selectedQuiz: Quiz
     lateinit var response: QuizResponse
-    var currentQuestion: Int = 0
-    val backend = Backend()
+    var currentQuestion = 1
+
+
+    init {
+        //dummy code
+
+        val dummyQuiz = Quiz("dummyQuiz")
+        val a = TrueFalseAnswer("dummyQuestion1", true)
+        val dummyQuestion = TrueFalseTriviaQuestion("dummyQuestion1", "The sky is blue:", a)
+        dummyQuiz.addQuestion(dummyQuestion)
+        Storage.createQuiz(dummyQuiz)
+
+        selectedQuizName = "dummyQuiz"
+        selectedQuiz = Storage.getQuiz(selectedQuizName)
+        response = QuizResponse(selectedQuizName)
+    }
 }
