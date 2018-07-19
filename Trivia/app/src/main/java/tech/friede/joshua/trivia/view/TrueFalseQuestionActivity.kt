e398283b201228be2057f773dc2a42d1894709a2
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-19 12:13:32 -0500

    offload next to superclass

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/view/TrueFalseQuestionActivity.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/view/TrueFalseQuestionActivity.kt
@@ -1,24 +1,17 @@
 package tech.friede.joshua.trivia.view
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_true_false_question.*
 import tech.friede.joshua.trivia.R
+import tech.friede.joshua.trivia.controller.Session
 
-class TrueFalseQuestionActivity : AppCompatActivity() {
+class TrueFalseQuestionActivity : QuizActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_true_false_question)
 
-        tf_question.text = MainActivity.selectedQuiz.questions[MainActivity.currentQuestion].question
+        tf_question.text = Session.selectedQuiz.getQuestionN(Session.currentQuestion)!!.question
     }
 
-    fun next(v: View) {
-        val i = Intent(this, Finished::class.java)
-        startActivity(i)
-        finish()
-    }
 }
