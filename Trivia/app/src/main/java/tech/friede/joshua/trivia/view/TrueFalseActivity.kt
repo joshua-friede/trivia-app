53649d2314bb877967013be80b64063c97d638eb
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-20 09:14:37 -0500

    rename for clarity

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/view/TrueFalseActivity.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/view/TrueFalseActivity.kt
@@ -1,7 +1,7 @@
 package tech.friede.joshua.trivia.view
 
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_true_false_question.*
+import kotlinx.android.synthetic.main.activity_true_false.*
 import tech.friede.joshua.trivia.R
 import tech.friede.joshua.trivia.controller.Session
 
@@ -9,9 +9,9 @@ class TrueFalseActivity : QuestionActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_true_false_question)
+        setContentView(R.layout.activity_true_false)
 
-        tf_question.text = Session.selectedQuiz.getQuestionN(Session.currentQuestion)!!.text
+        question_text.text = Session.selectedQuiz.getQuestionN(Session.currentQuestion)!!.text
     }
 
 }
