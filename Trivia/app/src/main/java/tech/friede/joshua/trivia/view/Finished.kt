d5204c28e2ae2b4c588a381eeb547000090d126e
Joshua Friede <joshua-friede@users.noreply.github.com>
2018-07-23 21:28:26 -0500

    show grade at end of quiz

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/view/Finished.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/view/Finished.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_finished.*
 import tech.friede.joshua.trivia.R
 import tech.friede.joshua.trivia.controller.Session
 import tech.friede.joshua.trivia.controller.Storage
@@ -17,6 +18,7 @@ class Finished : AppCompatActivity() {
 
         Storage.createResponse(Session.response)
         Log.d("responses", Storage.getResponses(Session.selectedQuizID).toString())
+        finished_message.text = "You scored ${Session.selectedQuiz.grade(Session.response)}/${Session.selectedQuiz.length()}"
     }
 
     fun reset(v: View) {
