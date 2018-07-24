a62591e92f9792864b3b51a8087ab931943a6099
Joshua Friede <hacker673@bogusemail.k3kabs.com>
2018-07-24 09:56:14 -0500

    squash a bug and optimize imports
    referenced data structure rather than activity

--- Trivia/app/src/main/java/tech/friede/joshua/trivia/view/QuestionActivity.kt
+++ Trivia/app/src/main/java/tech/friede/joshua/trivia/view/QuestionActivity.kt
@@ -3,12 +3,10 @@ package tech.friede.joshua.trivia.view
 import android.content.Intent
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_true_false.*
 import tech.friede.joshua.trivia.controller.Session
 import tech.friede.joshua.trivia.model.questions.MultipleChoice
 import tech.friede.joshua.trivia.model.questions.TrueFalse
@@ -21,7 +19,7 @@ abstract class QuestionActivity : AppCompatActivity() {
 
         when (Session.selectedQuiz.getQuestionN(Session.currentQuestion)) {
             is TrueFalse -> i.setClass(this, TrueFalseActivity::class.java)
-            is MultipleChoice -> i.setClass(this, MultipleChoice::class.java)
+            is MultipleChoice -> i.setClass(this, MultipleChoiceActivity::class.java)
             else -> i.setClass(this, Finished::class.java)
         }
         startActivity(i)
@@ -35,10 +33,10 @@ abstract class QuestionActivity : AppCompatActivity() {
         var snackbar = Snackbar.make(v, message, Snackbar.LENGTH_INDEFINITE).setAction("Next >") { next(v) }
         snackbar.view.layoutParams.width = FrameLayout.LayoutParams.MATCH_PARENT
         snackbar.view.layoutParams.height = FrameLayout.LayoutParams.WRAP_CONTENT
-        val sbText= snackbar.view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+        val sbText = snackbar.view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
         sbText.textSize = 32.toFloat()
         snackbar.view.findViewById<Button>(android.support.design.R.id.snackbar_action).textSize = 32.toFloat()
-        
+
         snackbar.show()
     }
 
